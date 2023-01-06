24f2da68919c8c4ee0b4b0a23cd4680d3a4c3bb1
andrehahn05 <andrehahn05@gmail.com>
2023-01-06 17:08:20 -0400

    Login_Activity

--- app/src/main/java/com/hahn/orgs/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/hahn/orgs/ui/activity/LoginActivity.kt
@@ -1,10 +1,12 @@
 package com.hahn.orgs.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.hahn.orgs.databinding.ActivityLoginBinding
 
-class LoginActivity:AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityLoginBinding.inflate(layoutInflater)
     }
@@ -12,5 +14,26 @@ class LoginActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setupBtnSignIn()
+        setupBtnSignUp()
     }
+    
+    private fun setupBtnSignIn() {
+        binding.activityLoginBtnSignIn.setOnClickListener {
+            val username = binding.activityLoginUsername.text.toString()
+            val passowrd = binding.activityLoginPassword.text.toString()
+            Log.i("LoginActivity", "onCreate: $username - $passowrd")
+            val intent = Intent(this, ListProductActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    
+    private fun setupBtnSignUp() {
+        binding.activityLoginBtnSignup.setOnClickListener {
+            val intent = Intent(this, FormUserRegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    
+    
 }
