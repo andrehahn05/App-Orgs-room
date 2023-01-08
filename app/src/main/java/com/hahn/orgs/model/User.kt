e10631b40a7ee4af274e913ab9cbf9e5619be5f3
andrehahn05 <andrehahn05@gmail.com>
2023-01-07 20:13:09 -0400

    model user

---
+++ app/src/main/java/com/hahn/orgs/model/User.kt
@@ -0,0 +1,12 @@
+package com.hahn.orgs.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class User(
+    @PrimaryKey
+    val id: String,
+    val name: String,
+    val password: String,
+)
