fbeab61f2aa7a19dd7f46fdef83b00c76a9ce1f9
andrehahn05 <andrehahn05@gmail.com>
2023-01-09 16:33:37 -0400

    UserDao

--- app/src/main/java/com/hahn/orgs/database/dao/UserDao.kt
+++ app/src/main/java/com/hahn/orgs/database/dao/UserDao.kt
@@ -2,10 +2,19 @@ package com.hahn.orgs.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.hahn.orgs.model.User
+import kotlinx.coroutines.flow.Flow
+
 
 @Dao
 interface UserDao {
     @Insert
     suspend fun store(user: User)
+    
+    @Query("SELECT * FROM User WHERE id = :userId AND password = :password")
+    suspend fun auth(userId: String, password:String): User?
+    
+    @Query("SELECT * FROM User WHERE id = :userId")
+    fun findById(userId: String) : Flow<User>
 }
