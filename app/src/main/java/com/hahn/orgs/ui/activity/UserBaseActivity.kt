a5bcfb02544df594e2bde5f7e12703056860ce4f
andrehahn05 <andrehahn05@gmail.com>
2023-01-11 16:16:26 -0400

    UserBaseActivity

---
+++ app/src/main/java/com/hahn/orgs/ui/activity/UserBaseActivity.kt
@@ -0,0 +1,58 @@
+package com.hahn.orgs.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.datastore.preferences.core.edit
+import androidx.lifecycle.lifecycleScope
+import com.hahn.orgs.database.AppDatabase
+import com.hahn.orgs.database.preferences.dataStore
+import com.hahn.orgs.database.preferences.userLoggedPreferences
+import com.hahn.orgs.extensions.navigateTo
+import com.hahn.orgs.model.User
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.launch
+
+
+abstract class UserBaseActivity: AppCompatActivity() {
+    
+    private val userDao by lazy {
+        AppDatabase.getInstance(this).userDao()
+    }
+    private var _user: MutableStateFlow<User?> = MutableStateFlow(null)
+    protected var user: StateFlow<User?> = _user
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState )
+        lifecycleScope.launch {
+            verifyUserLogin()
+        }
+    }
+    
+    private suspend fun verifyUserLogin(){
+        dataStore.data.collect { preferences ->
+            preferences[userLoggedPreferences]?.let { userId ->
+                getUser(userId)
+            } ?: navigateToLogin()
+        }
+    }
+    
+    private suspend fun getUser(userId: String) {
+        _user.value = userDao.findById(userId).firstOrNull()
+    }
+    
+    protected suspend fun logoutUser() {
+        dataStore.edit { preferences ->
+            preferences.remove(userLoggedPreferences)
+        }
+    }
+    
+    private fun navigateToLogin() {
+        navigateTo(LoginActivity::class.java) {
+            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        }
+        finish()
+    }
+}
